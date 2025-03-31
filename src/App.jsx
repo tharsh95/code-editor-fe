c57ae4bb58bd55fbde47d2e61e6d5e752ef48159
Harsh Tiwari <harsh@kimo.ai>
2025-04-01 04:41:48 +0530

    Update changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import JobsPage from './pages/JobsPage';
 import EditorPage from './pages/EditorPage';
 import QuestionsPage from './pages/QuestionsPage';
@@ -6,7 +6,7 @@ import './App.css';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className="flex flex-col h-screen">
         <Routes>
           <Route path="/" element={<JobsPage />} />
@@ -15,6 +15,6 @@ export default function App() {
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
